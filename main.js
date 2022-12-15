9420f6e79764a92c46caf144b60005a6a72eddd1
Tarık Yıldız <67359075+Tarik366@users.noreply.github.com>
2022-12-15 23:06:22 +0300

    Windows arayüz ayarlamaları

---
+++ main.js
@@ -0,0 +1,50 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+        color: '#2f3241',
+        symbolColor: '#74b1be',
+        height: 20
+    },
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+  nativeTheme.themeSource = 'dark'
+  win.setOverlayIcon('icons/overlay.jpg', 'Description for overlay')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.setUserTasks([
+    {
+      program: process.execPath,
+      arguments: '--new-window',
+      iconPath: process.execPath,
+      iconIndex: 0,
+      title: 'Wakaran',
+      description: 'Wakaranai kotoga aru'
+    }
+  ])
